2b6191d322e009304cc375be2cf6d71bc027970e
georgekvr <george.kvrivi@gmail.com>
2022-04-04 02:34:48 +0400

    change initial text

--- js/InitialText.js
+++ js/InitialText.js
@@ -1,45 +1,54 @@
-export const initText = `# Welcome to my Markdown Previewer!
-## This is a sub-heading...
-### And here's some other cool stuff:
-  
-Heres some code, \`<div></div>\`, between 2 backticks.
+export const initText = `# Insonuit erat dixit pendulaque cognitus vitis vixisti
 
-\`\`\`
-// this is multi-line code:
+## Tecta arcus hastamque evectus torvis tetigisse me
 
-function anotherExample(firstLine, lastLine) {
-  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
-    return multiLineCode;
-  }
-}
-\`\`\`
-  
-You can also make text **bold**... whoa!
-Or _italic_.
-Or... wait for it... **_both!_**
-And feel free to go crazy ~~crossing stuff out~~.
+Lorem markdownum aere soceri iuncique velox. Os nempe spatiis arma Tagus *per et
+omnesque*, est caput quamquam coniuge, quoque agitabilis parentem. Astypaleia et
+auras Cecropidum nostros, vultus facit sine, restabas quid sed Ixione.
 
-There's also [links](https://pedantic-johnson-e34bcc.netlify.app/), and
-> Block Quotes!
+- Numerabilis alta mollibus Astypaleia artes
+- Quem rettulit
+- Suae solvit Marathon serrae circum ne cervae
+- Percussis terga mansit pendent
+- Deusque inducta
+- Inmeriti tempusque neque rursus
 
-And if you want to get really crazy, even tables:
+Ad non coniugis: illius reor procellae. Rescierit duxerat fortissima fertis
+inconsumpta percussis sub iubet sic malorum tamen licet vocato gravemque nitor
+lacu dabantur tenebat.
 
-Wild Header | Crazy Header | Another Header?
------------- | ------------- | ------------- 
-Your content can | be here, and it | can be here....
-And here. | Okay. | I think we get it.
+> Alcyone rex primus miserabile timendi, numinis multis et et heros, sive causa!
+> In huic sonitu magne umet capillos posset, hic tellus nebulas; per nunc
+> insequitur ales, in.
 
-- And of course there are lists.
-  - Some are bulleted.
-     - With different indentation levels.
-        - That look like this.
+## Poplite Thoona
 
+Suae igne hunc parent. Genitor utve confundere vulnera. Adest truncaque aut
+oculos: possim vulnusque pectus adferre laborant erat cantat tamen. Habet
+praeside nec tamen inpensior, ab mea facies et, ab illa deorum meritasque moveri
+ferro purpureum, iamque. Crimine nec Baucis sine: quantumque negant invergens
+leones manus, orbe dextera pronus.
 
-1. And there are numbererd lists too.
-1. Use just 1s if you want! 
-1. But the list goes on...
-- Even if you use dashes or asterisks.
-* And last but not least, let's not forget embedded images:
+> Fronte et carmen spes [traiecit
+> licet](http://parteest.io/piasquemiserere.html) arvis Tagus voce erat
+> **invenio**: deorum. Nisi est mea crura, timetur sine nec, ut Faunine patet
+> illic obscurum. Flumina medium probarunt eque sive! Et [robore
+> aperit](http://innumeris.io/) volubilitas pervenit quoque.
 
-![JS Logo](https://www.thecodecrypt.com/images/2020/01/08/js.png)
+Bos torique *pariter*, cedere natam, taedia, duo super tantorum quos facies
+[Aenea](http://www.qualissecreta.org/deliuset.aspx). Te
+[toro](http://venusat.com/) auras in victae, canes magis columbas quem. Vertere
+genitor gemit ars et inania. Digitos hic; vidi auctor silvis nostras, sat
+plurima edita **tua** stagnum maximus spatiantur.
+
+- Recessit Aiax postquam Pasiphaen quodcumque erat
+- Pes effluxere in ducitur auras Tartara nativo
+- Tuis amnemque nexus
+- Plenissima usus Sabini atque proles
+- Palustris omnes habuit cognosci
+
+Fuerat **capillis**. Finge ad harenas [equos](http://vitaemoneo.org/), meorum
+finemque nunc sui cunctis in meritorum frustraque avidos latet fert infelix
+necat liquor. Et beatior nostra. Alphenor dederat Pleuron. Sustinet dolentes
+terrore, et ordine recentes.
 `;
